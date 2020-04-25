c1d1e08abc50b64972723166e8e52959607653b4
Hristo Ganev <hristo.ganev@pioneers.io>
2020-04-25 16:08:00 +0200

    feat: Implement Google auth

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,14 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { GoogleSigninDirective } from './google-signin.directive';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [GoogleSigninDirective, LoginPageComponent],
   imports: [
     CommonModule,
+    SharedModule,
     UserRoutingModule,
-    SharedModule
   ]
 })
 export class UserModule { }
